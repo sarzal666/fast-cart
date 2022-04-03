9ee156e3c39770580bc06d1536767f729a80476a
sarzixon <msarzala.work@gmail.com>
2022-04-03 16:54:59 +0200

    added react createRoot for v18 support

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Categories from "./Components/Categories/Categories";
-// import Cart from "./Components/Cart/Cart";
-// import Main from "./Components/Main/Main";
 
+const root = createRoot(document.getElementById('root'));
+root.render(<App />)
 
-ReactDOM.render(
-  // <React.StrictMode>
-    <App />,
-  // </React.StrictMode>,
-  document.getElementById('root')
-);
 
